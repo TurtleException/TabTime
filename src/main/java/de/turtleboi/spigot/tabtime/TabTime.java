aebe824310d00efb50283e70ced0bd126e3b383b
TurtleException <turtle.exception@eldritch.de>
2024-06-09 23:58:30 +0200

    Implement basically everything

--- src/main/java/de/turtleboi/spigot/tabtime/TabTime.java
+++ src/main/java/de/turtleboi/spigot/tabtime/TabTime.java
@@ -1,7 +1,45 @@
 package de.turtleboi.spigot.tabtime;
 
+import org.bukkit.World;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class TabTime extends JavaPlugin {
+    public static final String SYM_SUN = "§6☀";
+    public static final String SYM_MOON = "§9\uD83C\uDF19";
+    public static final String SYM_RAIN = "§b☂";
+    public static final String SYM_THUNDER = "§3⚡";
 
+    @Override
+    public void onEnable() {
+        // schedule updater
+        this.getServer().getScheduler().scheduleSyncRepeatingTask(this, () -> {
+            for (Player player : getServer().getOnlinePlayers()) {
+                player.setPlayerListFooter(toTime(player.getWorld()));
+            }
+        }, 1, 1);
+    }
+
+    public static String toTime(World world) {
+        int hours   = (int) ((world.getTime() / 1000 + 6) % 24);
+        int minutes = (int) ((world.getTime() % 1000) / (1000 / 60.0));
+
+        String weather;
+        if (world.isClearWeather()) {
+            if (hours >= 6 & hours < 18) {
+                weather = SYM_SUN;
+            } else {
+                weather = SYM_MOON;
+            }
+        } else if (world.isThundering()) {
+            weather = SYM_THUNDER;
+        } else {
+            weather = SYM_RAIN;
+        }
+
+        return "§7" + String.format("%02d", hours)
+                + "§8" + ":"
+                + "§7" + String.format("%02d", minutes)
+                + " " + weather;
+    }
 }
